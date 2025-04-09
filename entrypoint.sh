5242e384c5b6e313280c8b014d9918600948ce8a
Ktovoz <1053753518@qq.com>
2025-04-09 19:59:30 +0800

    fix: 修改文件名大小写

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-# 等待数据库
-echo "等待数据库启动..."
-sleep 5
-
 # 执行数据库迁移
 echo "执行数据库迁移..."
 python manage.py migrate
-
-# 收集静态文件
-echo "收集静态文件..."
-python manage.py collectstatic --noinput
 
 # 启动 Gunicorn
 echo "启动 Gunicorn 服务器..."
